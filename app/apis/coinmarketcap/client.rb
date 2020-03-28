950005483e703e7410e88fd8ee132f89082d85bd
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-03-28 23:34:07 +0100

    Create Coinmarketcap client fetching currencies ids and quotes.

---
+++ app/apis/coinmarketcap/client.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require './lib/coinmarketcap/connection.rb'
+
+module Coinmarketcap
+  class Client
+    COMMON_PATH = 'v1/cryptocurrency/'
+    IDS_PATH = 'map'
+    QUOTES_PATH = 'quotes/latest'
+
+    def initialize(api_key)
+      @header = {
+        'X-CMC_PRO_API_KEY' => api_key
+      }
+    end
+
+    def currencies_ids(currencies_string)
+      endpoint = COMMON_PATH + IDS_PATH
+
+      params = {
+        'symbol': currencies_string
+      }
+
+      get_request(endpoint, params)
+    end
+
+    def currencies_quotes(ids_string)
+      endpoint = COMMON_PATH + QUOTES_PATH
+
+      params = {
+        'id': ids_string
+      }
+
+      get_request(endpoint, params)
+    end
+
+    private
+
+    def get_request(endpoint, params)
+      Coinmarketcap::Connection.api.get(endpoint, params, @header)
+    end
+  end
+end
